5cab1649c371e393e1555b06005707219b68529a
xdc <1434412784@qq.com>
2020-04-11 18:42:37 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',  //照抄
-    app_id: "wx9b4283372e91c686",
-    app_secret: "6681fa52713303bfab26b47199a258ca",
-    t_app_id: "xkzpGyDd8aXUgLry15FmzYza",
-    t_app_secret: "JGS944m284z5GoRRH9vKutSU"
-  }
-})
