bdfdc94997bd7f95b5de6a5c032ea1df0de7edf2
xdc <1434412784@qq.com>
2020-04-12 17:05:38 +0800

    基本完成首页的增删改查，并修改了confirm组件，使其可以传值进入输入框

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,48 +3,79 @@ const {http} = require('../../lib/http')
 
 Page({
 
+  updateId: "",
+  updateIndex: "",
+
   /**
    * 页面的初始数据
    */
   data: {
-    visibleConfirm: false,
+    visibleCreateConfirm: false,
+    visibleUpdateConfirm: false,
     list: []
   },
-  confirmCreate(event) {
+  showConfirmCreate(event) {
     let content = event.detail
 
     if (content) {
-
-      console.log(content)
 
       http.post('/todos', {
         completed: false,
         description: content
       }).then(response => {
-        console.log(response.data)
-
         let todo = JSON.parse(response.data)["resource"]
 
         this.setData({list: this.data.list.concat(todo)})
-        this.hideConfirm()
+        this.hideCreateConfirm()
       })
     }
   },
-  hideConfirm() {
+  hideCreateConfirm() {
     this.setData({
-      visibleConfirm: false
+      visibleCreateConfirm: false
     })
   },
   showConfirm() {
-    this.setData({visibleConfirm: true})
+    this.setData({visibleCreateConfirm: true})
   },
   destroyTodo(event) {
     let index = event.currentTarget.dataset.index
+    let id = event.currentTarget.dataset.id
 
-    http.delete(`/todos?`)
+    http.put(`/todos/${id}`, {
+      completed: true
+    }).then(response => {
+      this.data.list[index] = JSON.parse(response.data)['resource']
+      this.setData({
+        list: this.data.list
+      })
+    })
 
     this.data.list[index].finished = true
     this.setData({list: this.data.list})
+  },
+  changeText(event) {
+    let {content, id, index} = event.currentTarget.dataset
+    this.updateId = id
+    this.updateIndex = index
+    this.setData({visibleUpdateConfirm: true,updateContent: content})
+  },
+  hideUpdateConfirm() {
+    this.setData({visibleUpdateConfirm: false})
+  },
+  showConfirmUpdate(event) {
+    const content = event.detail
+
+    http.put(`/todos/${this.updateId}`, {
+      description: content
+    }).then(response => {
+      this.data.list[this.updateIndex] = JSON.parse(response.data)['resource']
+      this.setData({
+        list: this.data.list
+      })
+
+      this.hideUpdateConfirm()
+    })
   },
 
   /**
@@ -65,7 +96,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    http.get('/todos').then(response => {
+    http.get('/todos?completed=false').then(response => {
       this.setData({list: JSON.parse(response.data)["resources"]})
     })
   },
